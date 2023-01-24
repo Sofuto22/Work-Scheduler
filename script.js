39237f3335aca73a14e80a6d271504ef57f97faf
Sofuto22 <zenigata19@gmail.com>
2023-01-24 16:43:19 -0500

    added time elements and event blocks

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 var displayCurrentDay = document.querySelector("currentDay");
 var currentDay= moment();
 displayCurrentDay.textContent = currentDay.format("dddd, MMMM Do YYYY");
+
+var blockTime = $(".time-block").addClass("row");
+var textBlock = $("<p>").addClass("description");
+blockTime.append(textBlock);
+
+var currentHours = parseInt(moment().format("H"));
+
+var loadEvents = function (slotsTime) {
+
+    slotsTime.forEach(element => {
+        console.log(element);
+        let text = localStorage.getItem(parseInt(element.time));
+        console.log(text);
+        if (text) {
+            element.text.val(text);
+        }
+        
+    });
+}
